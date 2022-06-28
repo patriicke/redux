663aec3fcfd748b41139c4eaf518e395fc516ed3
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-28 23:37:38 +0200

    This is about the redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,4 @@
 import { useEffect } from "react";
 import { createStore } from "redux";
-function App() {
-  const increment = () => {
-    return {
-      type: "INCREMENT"
-    };
-  };
-  const decrement = () => {
-    return {
-      type: "DECREMENT"
-    };
-  };
-  const counter = (state = 0, action) => {
-    switch (action.type) {
-      case "INCREMENT":
-        return state + 1;
-      case "DECREMENT":
-        return state - 1;
-    }
-  };
-  let store = createStore(counter);
-  useEffect(() => {}, [store]);
-  store.subscribe(() => console.log(store.getState()));
-  store.dispatch(increment());
-  store.dispatch(decrement())
-  return <div className="text-red-300">Hello World</div>;
-}
 
 export default App;
